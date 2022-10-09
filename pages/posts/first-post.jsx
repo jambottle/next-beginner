c8dc7453453d109d7c43a709b758e7ef68f74997
jambottle <jaewon.walks@gmail.com>
2022-10-09 22:45:07 +0900

    feat: Use Head component to modify the metadata of the page

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,8 +1,18 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+        <meta name="author" content="Chris Mills" />
+        <meta
+          name="description"
+          content="The MDN Web Docs Learning Area aims to provide complete beginners to the Web with all they need to know to get started with developing web sites and applications."
+        />
+      </Head>
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
