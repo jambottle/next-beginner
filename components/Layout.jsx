0426852a5bb91f2a55495cca3c42723afbeed991
jambottle <jaewon.walks@gmail.com>
2022-10-12 23:20:05 +0900

    design: Add util styles to Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,5 @@
 import styles from './Layout.module.css';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Layout({ children }) {
   return <div className={styles.container}>{children}</div>;
